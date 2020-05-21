5232ff7230b0fdfed4b5b7805a02e66a4f31ea1d
RuFi2k <filyk31@gmail.com>
2020-05-21 21:30:07 +0300

    Made some basic layouts, without class instances

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -34,8 +34,348 @@ class _MenuWidgetState extends State<MenuWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Testing menu render'),
+      body: Column(
+
+        children: <Widget>[
+          Container(
+            height: 250.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xFF9C0EEE), Color(0xFF3D02A0)],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Color(0xFFFFFFFF),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              color: Color(0x88000000),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Icon(
+                                Icons.ac_unit,
+                                color: Color(0xFF000000),
+                              ),
+                              Text(
+                                'sample text',
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.bold
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
