fd4ea5d38846ce3240cf84a8186dcb89bfc70198
RuFi2k <filyk31@gmail.com>
2020-05-22 19:20:07 +0300

    Horizontal scrollbar TODO

--- lib/UI/screens/menu.dart
+++ lib/UI/screens/menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:FlutterApp/UI/screens/categories.dart';
 import 'package:FlutterApp/UI/screens/header.dart';
@@ -21,8 +22,8 @@ class _MenuWidgetState extends State<MenuWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Color(0xFFF4FAFF),
+      body: ListView(
         children: <Widget>[
           Header(),
           Categories(),
@@ -41,21 +42,63 @@ class _MenuWidgetState extends State<MenuWidget> {
                 Padding(
                   padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
                   child: Container(
+                    height: 110.0,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
                         Container(
-                          height: 10.0,
-                          width: 10.0,
+                          width: 110.0,
                           decoration: BoxDecoration(
                             gradient: LinearGradient(
                               begin: Alignment.topLeft,
                               end: Alignment.bottomRight,
-                              colors: [Color(0xFFA151F2), Color(0xFFCB9EFA)]
+                              colors: [Color(0xFFA151F2), Color(0xFFCB9EFA)],
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                offset: Offset(2.0, 2.0),
+                                blurRadius: 10.0,
+                                spreadRadius: 1.0,
+                                color: Color(0x88000000),
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.payment,
+                                  color: Color(0xFFFFFFFF),
+                                  size: 25.0,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      '-\$40.00',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFFFFFFFF),
+                                        fontSize: 17.0,
+                                      )
+                                    ),
+                                    Text(
+                                        'Cash transfer',
+                                        style: TextStyle(
+                                          color: Color(0xFFFFFFFF),
+                                          fontSize: 13.0,
+                                        )
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
                           ),
-                          child: Text("Hello!"),
-                        )
+                        ),
                       ],
                     ),
                   ),
